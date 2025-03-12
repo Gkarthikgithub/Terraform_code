78dbf641c6c4d3b9def7814a0f9a4a9c6f6bcb8e
Gkarthikgithub <gallakarthik97@gmail.com>
2025-03-12 17:40:08 +0530

    changed vpc for external connectivity

--- Terraform_challenge/main.tf
+++ Terraform_challenge/main.tf
@@ -6,22 +6,55 @@ provider "aws" {
 
 
 
+# Create a VPC
 resource "aws_vpc" "main" {
   cidr_block = "10.0.0.0/16"
 }
 
+# Create Subnet 1 (Public)
 resource "aws_subnet" "subnet1" {
   vpc_id                  = aws_vpc.main.id
   cidr_block              = "10.0.3.0/24"
   availability_zone       = "us-east-1a"
   map_public_ip_on_launch = true
 }
+
+# Create Subnet 2 (Public)
 resource "aws_subnet" "subnet2" {
   vpc_id                  = aws_vpc.main.id
   cidr_block              = "10.0.2.0/24"
   availability_zone       = "us-east-1b"
   map_public_ip_on_launch = true
 }
+
+# Create an Internet Gateway
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.main.id
+}
+
+# Create a Route Table
+resource "aws_route_table" "public_rt" {
+  vpc_id = aws_vpc.main.id
+
+  # Default route to the Internet Gateway
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+}
+
+# Associate Route Table with Subnet 1
+resource "aws_route_table_association" "subnet1_association" {
+  subnet_id      = aws_subnet.subnet1.id
+  route_table_id = aws_route_table.public_rt.id
+}
+
+# Associate Route Table with Subnet 2
+resource "aws_route_table_association" "subnet2_association" {
+  subnet_id      = aws_subnet.subnet2.id
+  route_table_id = aws_route_table.public_rt.id
+}
+
 
 
 resource "aws_security_group" "web_sg" {
@@ -49,11 +82,13 @@ resource "aws_security_group" "web_sg" {
   }
 }
 
+
 resource "aws_instance" "web_server" {
   ami           = var.ami_id
   instance_type = var.instance_type
   subnet_id     = aws_subnet.subnet1.id
   vpc_security_group_ids = [aws_security_group.web_sg.id]
+  key_name               = "ci-cd"
   user_data = <<-EOF
               #!/bin/bash
               apt update -y
